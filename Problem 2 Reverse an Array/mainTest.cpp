bd478cb8522f2473646002cced70dc7b2dc060b9
aryajason <aryajasonr@gmail.com>
2024-09-27 14:57:26 +0700

    Update mainTest.cpp

--- Problem 2 Reverse an Array/mainTest.cpp
+++ Problem 2 Reverse an Array/mainTest.cpp
@@ -1,1 +1,31 @@
-asd
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+  // Hardcoded size
+	int n = 5; // the size of the vector
+	
+	vector<int> v = {2, 4, 6, 8, 10}; // declaring and initializing the vector v
+    int n = v.size(); // the size of the vector is determined from the initialization
+
+    reverse(v.begin(), v.end()); // reversing the vector
+
+    // Print the reversed vector
+    cout << "[";
+    for (int i = 0; i < n; i++) {
+    if (i == n - 1) {
+    cout << v[i] << "]"; // print the last element with closing bracket
+        } 
+    else 
+    {
+            cout << v[i] << ", "; // print the rest of the elements
+        }
+    }
+    cout << endl;
+		}
+    vector <int> v(n) expectedoutput = { 10 8 6 4 2 } 
+	} ;
+  if (expectedoutput==ans) cout <<"Succesfully"<<'/n';
+  else cout << "Failed" << '/n'
+}
